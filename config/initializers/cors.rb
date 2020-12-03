b60fb66e19c177471a2a613b7daafac2814dd127
Keegan Leitz <keegan@openbay.com>
2020-12-03 18:48:14 -0500

    expose headers

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -19,6 +19,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource '*',
       headers: :any,
       credentials: true,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      expose: ['Authorization', 'X-CSRF-Token', 'Content-Length', 'Content-Range']
   end
 end
