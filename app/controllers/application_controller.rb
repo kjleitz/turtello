652378b878fd45bfa4e78eaef84d29df612772fd
Keegan Leitz <keegan@openbay.com>
2020-12-03 20:25:45 -0500

    remove shitty shit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -98,19 +98,13 @@ class ApplicationController < ActionController::API
     unless authenticated?
       auth_header = request.headers['Authorization'].presence || ''
       auth_token = auth_header.gsub(/\Abearer +/i, '').strip
-      claims = JWT.decode(
-        auth_token,
-        Rails.application.secrets.secret_key_base,
-        false,
-        { algorithm: 'HS256' }
-      )
       claims_validated = JWT.decode(
         auth_token,
         Rails.application.secrets.secret_key_base,
         true,
         { algorithm: 'HS256' }
       )
-      Rails.logger.info("#require_auth! failed.\nAuth token: #{auth_token}\nClaims: #{claims}\nClaims validated: #{claims_validated}")
+      Rails.logger.info("#require_auth! failed.\nAuth token: #{auth_token}\nClaims validated: #{claims_validated}")
       render status: :unauthorized, json: json_error(:auth_token_invalid)
     end
   end
